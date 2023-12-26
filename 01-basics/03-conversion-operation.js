013e786b15d47a2a3f7b0ff65e30efb6c5cb824e
Fathima-shah <143911874+Fathima-shah@users.noreply.github.com>
2023-12-26 08:55:48 +0000

    conversion of data types

---
+++ 01-basics/03-conversion-operation.js
@@ -0,0 +1,18 @@
+let score = "name"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33xyz" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
