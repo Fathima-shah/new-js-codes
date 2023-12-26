aff07ef0b6c55d4e5a14e7c2565504b981ca0919
Fathima-shah <143911874+Fathima-shah@users.noreply.github.com>
2023-12-26 08:34:51 +0000

    done with basics

---
+++ 01-basics/01-variable.js
@@ -0,0 +1,4 @@
+const accountId = 124358
+let accountEmail = "fathi@gmail.com"
+
+console.table([accountId, accountEmail])
---
+++ 01-basics/02-data-types.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol = unique
+
+// object
