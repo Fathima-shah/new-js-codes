287e211ed583836b804a7db1043fa8ad526a86db
Fathima-shah <143911874+Fathima-shah@users.noreply.github.com>
2023-12-29 15:00:16 +0000

    covered strings basics

---
+++ 01-basics/04-string.js
@@ -0,0 +1,44 @@
+const name = "shana"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+console.log(`hello my name is ${name} and my repocount is${repoCount}`);
+
+
+const gameName = new String('shan-af');
+
+//console.log(gameName[0])
+console.log(gameName.__proto__)
+
+//console.log(gameName.length);
+console.log(gameName.toUpperCase(3))
+//console.log(gameName.charAt(3))
+
+//const newString = gameName.substring(0, 3);
+
+const anotherString = gameName.slice(-6, 2);
+console.log(anotherString);
+
+//console.log(newString);
+
+
+
+const oneString = "   javascript  "
+
+console.log(oneString);
+console.log(oneString.trim()); //.trim() elivates the extra spaces and evrything
+
+ const url = "http://fathi.com/fathi%20shana"
+
+ console.log(url.replace('20', '-')); // u can Use this function to replace anything
+
+ console.log(url.includes('mari')); // this cfunction can be used to check if it includes certain keywords..and it will be confirmd with true or false
+
+ 
+
+
+
+
+
+
