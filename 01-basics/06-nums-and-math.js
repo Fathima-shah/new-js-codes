f8f37362f33a9d788f55a23b8d5da3d85008b4db
Fathima-shah <143911874+Fathima-shah@users.noreply.github.com>
2023-12-30 09:17:29 +0000

    math and numbers

---
+++ 01-basics/06-nums-and-math.js
@@ -0,0 +1,36 @@
+const score = 100;
+
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance.toFixed(1));
+
+ const otherNumber = 123.87665
+ //console.log(otherNumber.toPrecision(3)) // it will give the precise value
+
+
+ const hundreds = 1000000000
+
+ //console.log(hundreds.toLocaleString('en-IN'))
+
+ //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+ //console.log(Math);
+// console.log(Math.abs(-4))
+ //console.log(Math.round(23.766))
+ //console.log(Math.ceil(23.276)) // it will only give top value that is precisely 24
+ //console.log(Math.floor(23.987)) // it will only give the lower value which is 23
+ //console.log(Math.min(2, 4, 5, 6))
+
+// console.log(Math.random())
+ //console.log(Math.floor(Math.random() *10)+1)
+
+ const min = 10;
+ const max = 30;
+
+ console.log(Math.floor(Math.random() * (max-min+1))+ min)
+
+
+
+
