bb45eab35049abcf8ea9a96b6d1cc2ee5b091503
Fathima-shah <143911874+Fathima-shah@users.noreply.github.com>
2024-01-06 09:33:52 +0000

    if else

---
+++ 04-control-flow/one.js
@@ -0,0 +1,37 @@
+//if 
+
+const isUserLoggedIn = true
+const temperature = 40
+
+// if(temperature === 39)
+// {
+//     console.log("less than 50")
+// } else{
+//     console.log("greater than 50")
+
+
+// <, >, <=, >=, ==, ===, !=, !==
+
+const score = 200
+
+if(score > 100) {
+    let power = "fly"
+    //console.log(`user power: ${power}`)
+}
+
+const balance = 1000
+ if (balance>500) console.log("test");
+
+ //if else
+
+//  if(isUserLoggedIn && temperature){
+//     console.log("allow access")
+//  }
+
+//  if(isUserLoggedIn && temperature && 2==3){
+//     console.log("allow access")
+//  }
+
+if(isUserLoggedIn || password){
+    console.log("allow")
+}
